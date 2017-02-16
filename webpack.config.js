d111cee816f7c7c3f84e2bfa30560561cbfd5189
Josh Gribbon <jgribbon@stevens.edu>
2017-02-15 21:04:17 -0500

    Set up webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/*
+    This is the configuration file for webpack
+*/
+const path = require('path');
+
+//This handles script tag insertions for bundles
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+          { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+          { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+}
