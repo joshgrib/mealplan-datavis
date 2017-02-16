8f22d7ba2b9b2e2b5fb1c395ed3f9fd4dcbfabfa
Josh Gribbon <jgribbon@stevens.edu>
2017-02-15 22:07:09 -0500

    Add meal and plan data, app is now a functioning basic todo app

---
+++ client/components/TodoApp.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div>
+       <div>
+          <h1>to-do</h1>
+       </div>
+    </div>
+  );
+}
+
+const TodoForm = ({addTodo}) => {
+  // Input Tracker
+  let input;
+  // Return JSX
+  return (
+    <div>
+      <input ref={node => {
+        input = node;
+      }} />
+      <button onClick={() => {
+        addTodo(input.value);
+        input.value = '';
+      }}>
+        +
+      </button>
+    </div>
+  );
+};
+
+const Todo = ({todo, remove}) => {
+  // Each Todo
+  return (<li onClick={() => {remove(todo.id)}}>{todo.text}</li>);
+}
+
+const TodoList = ({todos, remove}) => {
+  // Map through the todos
+  const todoNode = todos.map((todo) => {
+    return (<Todo todo={todo} key={todo.id} remove={remove}/>)
+  });
+  return (<ul>{todoNode}</ul>);
+}
+
+// Contaner Component
+// Todo Id
+window.id = 0;
+export default class TodoApp extends React.Component{
+  constructor(props){
+    // Pass props to parent class
+    super(props);
+    // Set initial state
+    this.state = {
+      data: []
+    }
+  }
+  // Add todo handler
+  addTodo(val){
+    // Assemble data
+    const todo = {text: val, id: window.id++}
+    // Update data
+    this.state.data.push(todo);
+    // Update state
+    this.setState({data: this.state.data});
+  }
+  // Handle remove
+  handleRemove(id){
+    // Filter all todos except the one to be removed
+    const remainder = this.state.data.filter((todo) => {
+      if(todo.id !== id) return todo;
+    });
+    // Update state with filter
+    this.setState({data: remainder});
+  }
+
+  render(){
+    // Render JSX
+    return (
+      <div>
+        <Title />
+        <TodoForm addTodo={this.addTodo.bind(this)}/>
+        <TodoList
+          todos={this.state.data}
+          remove={this.handleRemove.bind(this)}
+        />
+      </div>
+    );
+  }
+}
+//ReactDOM.render(<TodoApp />, document.getElementById('container'));
